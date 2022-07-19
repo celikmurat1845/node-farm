a7158e7c16ae762f043115806fb3ebff10333d27
Murat Çelik <celikmurat1845@gmail.com>
2022-07-19 21:14:29 +0300

    ADD package

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate.js');
 
 ///////////////////////////////////////
@@ -37,6 +39,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slug = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slug);
 
 const server = http.createServer((req, res) => {
 // const pathName = req.url;
